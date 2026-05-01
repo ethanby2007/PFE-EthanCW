d8c3c1e26538c4fdf1794541756ba744ef342f24
ethanby2007 <ethan2.benyehuda@live.uwe.ac.uk>
2026-05-01 18:50:53 +0100

    Created io.c, and implemented file-handling logic. Safeguard added to check if file exists before trying to read it.

---
+++ io.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "waveform.h"
+
+int load_csv(const char *filename, waveformSample *samples, int n) {
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL) {
+        return 0;
+    }
+
+    char header[1024];
+    fgets(header, sizeof(header), file);
+
+    for (int i = 0; i < n; i++) {
+        if (fscanf(file, "%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf",
+            &samples[i].tStamp,
+            &samples[i].phaseVoltageA,
+            &samples[i].phaseVoltageB,
+            &samples[i].phaseVoltageC,
+            &samples[i].lineCurrent,
+            &samples[i].freq,
+            &samples[i].powerFactor,
+            &samples[i].thdPercent) !=8) {
+        break;
+        }
+    }
+    fclose(file);
+    return 1;
+}
